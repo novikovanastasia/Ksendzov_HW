4ea890d6812ae79c011d4fef364c541561f02338
Анастасия Новикова <novikovanastasia@mail.ru user=anastasia>
2022-06-16 18:46:47 +0300

    add hw3

--- hw2sql.sql
+++ hw2sql.sql
@@ -1,9 +1,11 @@
+-- 1.Создать таблицу employees- id. serial,  primary key, employee_name. Varchar(50), not null
+
 create table employees (
 id serial primary key,
 employee_name Varchar(50) not null);
  
 select * from employees;
-
+--2.Наполнить таблицу employee 70 строками.
 insert into employees (employee_name)
 values ('emloyee1'),
        ('emloyee2'),
@@ -75,13 +77,15 @@ values ('emloyee1'),
        ('emloyee68'),
        ('emloyee69'),
        ('emloyee70');
-       
-      
+       select * from employees;
+
+     -- 3. Создать таблицу salary- id. Serial  primary key,- monthly_salary. Int, not null
+
       create table salary_1 (
       id serial primary key,
       monthly_salary int not null 
      );
-      
+    --4)Наполнить таблицу salary 15 строками:
      insert into salary_1 (monthly_salary)
       values (1000),
              (1100), 
@@ -99,15 +103,20 @@ values ('emloyee1'),
              (2300),
              (2400),
              (2500);
-             
+             select * from salary_1;
+            
  drop table emloyee_salary;
 
+--5.Cоздать таблицу employee_salary(- id. Serial  primary key,
+-- employee_id. Int, not null, unique
+-- salary_id. Int, not null)
  create table employee_salary (
  id serial primary key,
  employee_id int not null unique,
  salary_id int not null
  );
-
+--6.Наполнить таблицу employee_salary 40 строками:
+-- в 10 строк из 40 вставить несуществующие employee_id
  insert into employee_salary (employee_id, salary_id)
  values      (1,2),
              (2,4), 
@@ -149,15 +158,19 @@ values ('emloyee1'),
              (79,15),
              (80,1);
              
+select * from employee_salary;
 
-
+--7)Создать таблицу roles
+-- id. Serial  primary key,
+-- role_name. int, not null, unique
 create table roles(
 id serial primary key, 
 role_name int not null unique 
 ); 
+--8)Поменять тип столба role_name с int на varchar(30)
 alter table roles
 alter column role_name type varchar(30);
-
+--9)Наполнить таблицу roles 20 строками
 insert into roles (role_name)
 values ('Junior Python developer'),
        ('Middle Python developer'),
@@ -179,7 +192,10 @@ values ('Junior Python developer'),
        ('Middle Automation QA engineer'),
        ('Senior Automation QA engineer');
       
-      
+     --10)Создать таблицу roles_employee
+--- id. Serial  primary key,
+-- employee_id. Int, not null, unique (внешний ключ для таблицы employees, поле id)
+-- role_id. Int, not null (внешний ключ для таблицы roles, поле id) 
     create table roles_employee (
     id serial primary key,
     employee_id int not null unique, 
@@ -189,7 +205,7 @@ values ('Junior Python developer'),
     foreign key (role_id)
     references roles (id)
    );
-   
+   --11)Наполнить таблицу roles_employee 40 строками:
    insert into roles_employee (employee_id,role_id)
    values    (1,2),
              (2,4), 
